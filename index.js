45b84d9c3e1a80d5bcedb4e707081320b6752fe1
Rich Hildred <rhildred@gmail.com>
2015-04-03 17:39:07 -0400

    added scope object

--- index.js
+++ index.js
@@ -3,26 +3,34 @@ var express = require("express"),
     server = require('http').createServer(app),
     io = require('socket.io')(server),
     _ = require('underscore'),
-    debug=require("debug")("index.js");
+    debug = require("debug")("index.js");
 
 var ipaddr = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || parseInt(process.argv.pop()) || 8080;
 
 debug(ipaddr, port);
 
-server.listen(port, ipaddr, function(){
+server.listen(port, ipaddr, function () {
     console.log("Server listening at port %d", port);
 });
 
-io.on('connection', function (socket) {
-  // when the client emits 'receive message', this listens and executes
-  socket.on('receive message', function (data) {
-      var oOut = {
-          from: _.escape(data.from),
-          message: _.escape(data.message)
-      };
-      socket.broadcast.emit("receive message", oOut);
-  });
-});
+var $scope = {
+    init: function () {
+        io.on('connection', $scope.connection);
+    },
+    connection: function (socket) {
+        socket.on('receive message'
+            $scope.receive);
+    },
+    receive: function (data) {
+        var oOut = {
+            from: _.escape(data.from),
+            message: _.escape(data.message)
+        };
+        socket.broadcast.emit("receive message", oOut);
+    }
+};
+$scope.init();
+
 
 app.use(express.static(__dirname + '/www/'));
