9fb05c0831f34f40b0f798bedef015f5e59deca1
Rich Hildred <rhildred@gmail.com>
2015-04-04 00:49:20 -0400

    started to dp email verification thing

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ var express = require("express"),
     io = require('socket.io')(server),
     _ = require('underscore'),
     debug = require("debug")("index.js"),
-    extend = require("extend");
+    extend = require("extend"),
+    md5 = require('MD5'),
+    oCreds = require('../data/sendgrid.json'),
+    https = require('https'),
+    bodyParser = require('body-parser'),
+    querystring = require('querystring');
+
+app.use( bodyParser.json() );
 
 var ipaddr = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || parseInt(process.argv.pop()) || 8080;
@@ -51,6 +58,67 @@ var $scope = {
 
 };
 $scope.init();
+
+app.post('/sendemail', function(req, response){
+    var sEmail = req.body.email;
+    var sCode = md5(sEmail + ':' + oCreds.realm).substr(0,4);
+    var sMessage = '<p>Thank-you for your interest in tellme!</p><p>Please enter this code: ' + sCode + ' to verify your email</p>';
+    var sUrl = "https://api.sendgrid.com/api/mail.send.json";
+    var sSender = "noreply@salesucation.com";
+    var oData = {
+        from: sSender,
+        to: sEmail,
+        subject: 'see included code ... thank-you for your interest',
+        html: sMessage,
+        replyto: sSender,
+        api_user: oCreds.account,
+        api_key: oCreds.password
+    };
+    var post_data = querystring.stringify(oData);
+    debug(post_data);
+
+    // An object of options to indicate where to post to
+    var post_options = {
+        host: 'api.sendgrid.com',
+        path: '/api/mail.send.json',
+        method: 'POST',
+        headers: {
+            "User-Agent": "node.js",
+            Accept: "*/*",
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+    };
+
+    // Set up the request
+    var post_req = https.request(post_options, function (res) {
+        debug("statusCode: ", res.statusCode);
+        debug("headers: ", res.headers);
+        var sData = "";
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            debug('Response: ' + chunk);
+            sData += chunk;
+        });
+        res.on('end', function () {
+            response.end('{"result":"' + sData + '"}');
+        });
+    });
+
+    // post the data
+    post_req.write(post_data);
+    post_req.end();
+
+});
+
+app.post('/verifyemail', function(req, res){
+    var sEmail = req.body.email;
+    var sCode = req.body.code;
+    if(sCode == md5(sEmail + ':' + oCreds.realm).substr(0,4)){
+        res.end('{"result":"success"}');
+    }else{
+        res.end('{"result":"failure"}');
+    }
+});
 
 
 app.use(express.static(__dirname + '/www/'));
